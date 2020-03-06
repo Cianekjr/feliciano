28aec5e9c4cfe241274bb7a55689bc25c04edb82
Mateusz Chrzanowski <41521483+Cianekjr@users.noreply.github.com>
2020-03-06 22:59:01 +0100

    feat(Services): Add Services component

--- pages/about.vue
+++ pages/about.vue
@@ -1,27 +1,26 @@
 <template>
   <div class="wrapper">
     <Header :bg-index="2" title="About" half />
-    <container class="container">
-      <Heading heading="Services" subheading="Catering Services" />
-    </container>
+    <Services class="services" />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from '@vue/composition-api'
-import container from '~/layouts/container.vue'
+
 import Header from '~/components/Header.vue'
-import Heading from '~/components/Heading.vue'
+import Services from '~/components/Services.vue'
 
 export default defineComponent({
   components: {
-    container,
     Header,
-    Heading
+    Services
   }
 })
 </script>
 
 <style>
-
+  .services {
+    background-color: #fafafa;
+  }
 </style>
